77c04823a376afd0358417baf8ab78d4bb0547ab
AbhilashKotha <113061137+AbhilashKotha@users.noreply.github.com>
2023-08-11 17:51:32 -0500

    created a new UI without changing backend

---
+++ new_user_interface/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace UIRemote;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
